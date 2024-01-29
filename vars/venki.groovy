24a01d95ee86f6ce8bea8814b63f4b7db7316710
venkataramanaangadala <155218269+venkataramanaangadala@users.noreply.github.com>
2024-01-29 13:09:10 +0530

    Update venki.groovy

--- vars/venki.groovy
+++ vars/venki.groovy
@@ -9,6 +9,14 @@ def gitBuild()
 }
 
 
+def gitDeployment(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+
+}
+
+
+
 
 
 
